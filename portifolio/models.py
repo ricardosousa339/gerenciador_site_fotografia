78aa3ac8c31012d92128a47681a0e74085543757
Ricardo Henrique <ricardosousa339@gmail.com>
2025-02-21 17:31:32 -0300

    Modificados models da aplicação

--- portifolio/models.py
+++ portifolio/models.py
@@ -2,29 +2,73 @@ from django.db import models
 from django.conf import settings
 
 from core.models import Base
+from portifolio.files import generate_unique_file_name
+from portifolio.validators import FileSizeValidator
+from django.core.validators import FileExtensionValidator
 from usuario.models import Usuario
 
 class Album(Base):
     titulo = models.CharField(max_length=255)
+    url = models.CharField(max_length=255, blank=True, null=True)
+    categoria = models.ForeignKey(
+        "Categoria",
+        on_delete=models.SET_NULL,
+        related_name='albums',
+        blank=True,
+        null=True
+    )
     usuario = models.ForeignKey(
         Usuario,
         on_delete=models.CASCADE,
         related_name='albums'
+    )
+    capa = models.ForeignKey(
+        "Foto",
+        on_delete=models.SET_NULL,
+        related_name='cover',
+        blank=True,
+        null=True
     )
 
     
     class Meta:
         verbose_name = "Álbum"
         verbose_name_plural = "Álbuns"
-        fields_display = ["titulo", "usuario"]
+        fields_display = ["titulo", "usuario", "url", "categoria"]
         # icon_model = "fas fa-user"
 
     def __str__(self):
         return self.title
 
+class Categoria(Base):
+    nome = models.CharField(max_length=255)
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+    
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        fields_display = ["nome", "usuario"]
+        # icon_model = "fas fa-user"
+
+    def __str__(self):
+        return self.name
 class Foto(Base):
     nome = models.CharField(max_length=255)
-    url = models.URLField(max_length=500)
+    arquivo = models.ImageField(
+        "Arquivo",
+        upload_to=generate_unique_file_name,
+        blank=True,
+        null=True,
+        help_text="Arquivo da foto",
+        validators=[
+            FileExtensionValidator(allowed_extensions=["pdf", "jpg", "jpeg", "png"]),
+            FileSizeValidator(max_size=5),
+        ],
+    )
     album = models.ForeignKey(
         Album,
         on_delete=models.CASCADE,
@@ -39,3 +83,21 @@ class Foto(Base):
 
     def __str__(self):
         return self.name
+    
+class ImagemHeader(Base):
+    imagem = models.ForeignKey(
+        Foto,
+        on_delete=models.CASCADE,
+        related_name='header'
+    )
+    usuario = models.ForeignKey(
+        Usuario,
+        on_delete=models.CASCADE,
+        related_name='headers'
+    )
+
+    class Meta:
+        verbose_name = "Imagem de Cabeçalho"
+        verbose_name_plural = "Imagens de Cabeçalho"
+        fields_display = ["imagem", "usuario"]
+        # icon_model = "fas fa-user"
