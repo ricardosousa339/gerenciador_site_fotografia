0776b3e7725816cc9368de6bc452e4e83fa04edd
Ricardo Henrique <ricardosousa339@gmail.com>
2025-03-06 20:59:46 -0300

    Add FatoSobreSerializer to SecaoSobreSerializer for related facts

--- portifolio/api/serializers/secaosobre.py
+++ portifolio/api/serializers/secaosobre.py
@@ -1,6 +1,7 @@
 from drf_jsonmask.serializers import FieldsListSerializerMixin
 from rest_framework.serializers import ModelSerializer
 
+from portifolio.api.serializers.fatosobre import FatoSobreSerializer
 from portifolio.api.serializers.foto import FotoSerializer
 from portifolio.models import SecaoSobre
 
@@ -8,6 +9,7 @@ from portifolio.models import SecaoSobre
 class SecaoSobreSerializer(FieldsListSerializerMixin, ModelSerializer):
     """Class do serializer do model SecaoSobre para o POST, PUT, PATCH, DELETE"""
     imagem = FotoSerializer(many=False, read_only=True)
+    fatos_sobre = FatoSobreSerializer(many=True, read_only=True)
     class Meta:
         model = SecaoSobre
         exclude = ["deleted", "enabled"]
