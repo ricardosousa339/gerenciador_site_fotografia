9947415969755b26705ad76fd9015d9cf94ec96b
Ricardo Henrique <ricardosousa339@gmail.com>
2025-03-08 11:01:47 -0300

    Refactor CORS allowed origins to use configuration variable

--- base/settings.py
+++ base/settings.py
@@ -68,9 +68,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "base.urls"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8081", "https://ricardosousa339.github.io",
-]
+CORS_ALLOWED_ORIGINS = config("CORS_ALLOWED_ORIGINS", cast=Csv(), default="http://localhost:8081")
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
